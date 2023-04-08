435f806c1e67e330c010bd4a95c7e20b416665f4
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-04-08 14:21:09 -0300

    add log in

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+const passport = require('passport')
 
 router.get('/register', (req, res) => {
 	res.render('auth/register')
@@ -11,6 +12,16 @@ router.post('/register', async (req, res) => {
 	const newUser = new User({ email, username })
 	await User.register(newUser, password)
 	req.flash('success', `Welcome, ${username}`)
+	res.redirect('/products')
+})
+
+router.get('/login', (req, res) => {
+	res.render('auth/login', { error: req.flash('error') })
+})
+
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), (req, res) => {
+	const { username } = req.body
+	req.flash('success', `Welcome back, ${username}`)
 	res.redirect('/products')
 })
 
